1444530673de4b14eb6b8329cdeaf809562b8eb8
Andrewy-gh <andrewydev6@gmail.com>
2026-05-04 10:44:04 -0400

    feat: create Effect server

---
+++ src/http/server.ts
@@ -0,0 +1,17 @@
+import { NodeHttpServer } from "@effect/platform-node";
+import { Effect, Layer } from "effect";
+import { HttpRouter, HttpServer } from "effect/unstable/http";
+import { createServer } from "node:http";
+import { HttpRoutes } from "./routes.js";
+import { AppConfigService } from "../config.js";
+
+const NodeServerLive = Layer.effect(HttpServer.HttpServer)(
+  Effect.gen(function* () {
+    const config = yield* AppConfigService;
+    return yield* NodeHttpServer.make(createServer, { port: config.port });
+  }),
+).pipe(Layer.provide(NodeHttpServer.layerHttpServices));
+
+export const HttpServerLive = HttpRouter.serve(HttpRoutes).pipe(
+  Layer.provide(NodeServerLive),
+);

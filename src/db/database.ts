8398a5044d2f3c16b718e5aa697995fe47da8688
Andrewy-gh <andrewydev6@gmail.com>
2026-05-03 23:13:45 -0400

    feat: migrate database.ts to Effect

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,12 +1,29 @@
+import { Context, Effect, Layer } from "effect";
 import { Kysely, PostgresDialect } from "kysely";
 import { Pool } from "pg";
-import { config } from "../config.js";
+import { AppConfigService } from "../config.js";
 import { DB } from "./generated.js";
 
-export const db = new Kysely<DB>({
-  dialect: new PostgresDialect({
-    pool: new Pool({
-      connectionString: config.databaseUrl,
-    }),
-  }),
+export class Database extends Context.Service<Database, Kysely<DB>>()(
+  "Database",
+) {}
+
+const makeDatabase = Effect.gen(function* () {
+  const config = yield* AppConfigService;
+
+  return yield* Effect.acquireRelease(
+    Effect.sync(
+      () =>
+        new Kysely<DB>({
+          dialect: new PostgresDialect({
+            pool: new Pool({
+              connectionString: config.databaseUrl,
+            }),
+          }),
+        }),
+    ),
+    (db) => Effect.promise(() => db.destroy()),
+  );
 });
+
+export const DatabaseLive = Layer.effect(Database)(makeDatabase);
